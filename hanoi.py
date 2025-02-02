e0685a2f0c7e61f5ced18d71dc148df8d2571271
WingTillDie <mychat.01a2bc3def@gmail.com>
2025-02-02 17:35:05 +0800

    Add main program

---
+++ hanoi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Recursion function
+def hanoi(s: str, d: str, t: str, n: int, n_max: int) -> None:
+    # s: source peg
+    # d: destination peg
+    # t: temporary peg, intermediatary peg
+    # n: peg size
+    # n_max: maximum peg size
+    
+    # Boundary condition
+    if n == 0: return
+    hanoi(s=s, d=t, t=d, n=n-1, n_max=n_max)
+    for _ in range(n_max-n):
+        print('  ', end='')
+    print(f'{s}{d}')
+    hanoi(s=t, d=d, t=s, n=n-1, n_max=n_max)
+
+hanoi(s='0', d='2', t='1', n=4, n_max=4)
+
